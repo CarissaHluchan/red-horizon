413c5b00f3c5b4a10681e48c2b9c166fc9275ad3
Charles B. <charlesbanks238@gmail.com>
2024-09-04 18:57:32 -0700

    Add: project file structure and updated apicalls file.
    Co-authored-by: Carissa Hluchan <carissahluchan@gmial.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Charles Banks <charlesbanks238@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApiCalls from './ApiCalls/ApiCalls'
 // console.log(process.env.REACT_APP_API_KEY, '<-- API KEY');
 
-function App() {
+async function App() {
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=1995-06-16`)
-    .then(response => response.json())
-    .then(data => console.log(data, '<-- HERE'))
-    .catch(err => console.log(err))
+  // fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=1995-06-16`)
+  //   .then(response => response.json())
+  //   .then(data => console.log(data, '<-- HERE'))
+  //   .catch(err => console.log(err))
+
+  // let nasaStuff = await fetch(`https://images-api.nasa.gov/search?q=mars`)
+  //                   .then(response => response.json())
+  //                   .then(data => console.log(data.collection.items, '<-- HERE'))
+  //                   .catch(err => console.log(err))
+
+  let fetchNasaStuff = await fetch(`https://images-api.nasa.gov/search?q=mars`)
+  let nasaStuffFetched = await fetchNasaStuff.json()
+  let nasaStuff = nasaStuffFetched.collection.items
+
+  // console.log(nasaStuff, '<-- HERE')
+
+  let finalNasaStuff = []
+
+  for(let i of nasaStuff) {
+    console.log(i.data[0].keywords.includes('rover'), 'FOR FINDING KEYWORDS')
+    if (i.data[0].keywords.includes('rovers')) {
+      console.log(i, 'THIS IS i')
+      finalNasaStuff.push(i)
+    }
+  }
+
+  console.log(finalNasaStuff, '<-- TING')
 
 
   return (
