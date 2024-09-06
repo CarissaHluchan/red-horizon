4289fb6ef57efb1b727c9581f292f18cf5ddd4d1
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-05 19:27:24 -0600

    Add: component files and directories
    Co-authored-by: Charles Banks <charlesbanks238@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; // do we need this?
 import './App.css';
 import ApiCalls from './ApiCalls/ApiCalls'
 // console.log(process.env.REACT_APP_API_KEY, '<-- API KEY');
@@ -36,22 +35,8 @@ async function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    </main>
   );
 }
 
