95a8be2976fbcd1ece5e63ff2cf7b28984f0381f
Carissa Hluchan <carissahluchan@gmail.com>
2024-09-09 18:37:15 -0600

    Fix: changes to SingleMediaDetails
    Co-authored-by: Charles Banks <charlesbanks238@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>

--- src/components/SingleMediaDetails/SingleMediaDetails.tsx
+++ src/components/SingleMediaDetails/SingleMediaDetails.tsx
@@ -16,11 +16,16 @@ interface SingleMediaDetailsProps {
 function SingleMediaDetails({ data }: SingleMediaDetailsProps) {
   return (
     <div className='single-media-detail-wrapper'>
+      <Link to='/allMarsMedia' className='slingle-media-back-button'>Back to All Mars Media</Link>
       <div className='single-media-title'>{data.title}</div>
       <img className='single-media-main-image' src={data.img_src} alt={data.description} />
       <p className='single-media-description'>{data.description}</p>
       <div className='single-media-date'>{data.date_created}</div>
-      <Link to="/allMarsMedia" className='slingle-media-back-button'>Back to All Mars Media</Link>
+      <div className='radio-button-parent'>
+        <label>Add to favorites</label>
+        <input type="radio" className='favorite-radio-button' />
+      </div>
+      <Link to='/favorites' className='see-favorites'>See Favorites</Link>
     </div>
   );
 }
